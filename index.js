c90ec927467a7b445a61414d33d97c0bea7dad06
Abdullah <ssmmhazz@gmail.com>
2022-03-15 23:06:53 +0500

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const bodyParser = require("body-parser");
+const app = express()
+app.use(bodyParser.json({limit: '5000kb'}));
 
-app.get('/',(res,req)=>{
-    console.log('asdf')
-res.send('asd')
+var todos = [];
+
+app.get('/', function (req, res) {
+    console.log('Hello')
+    res.send('Hello World')
 })
+
+app.use(function (req, res, next) {
+    console.log('Hello');
+    console.log(req.query);
+    if(req.query.key === '123456'){
+        next();
+    }
+    else {
+        res.send("Invalid API key");
+    }
+});
+
+
+app.get('/get', function (req, res) {
+    res.send(todos);
+});
+
+app.post('/add', function (req, res) {
+    console.log(req.body);
+    todos.unshift(req.body);
+    res.send('Successfully added')
+})
+
+app.listen(process.env.PORT || 3000)
